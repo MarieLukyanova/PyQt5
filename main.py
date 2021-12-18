4cf83d98d371fd1476bb835d128887811cc17ab8
Ivan <st.affeev@yandex.ru>
2021-12-18 23:45:13 +0300

    fixed

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import sys
 
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QErrorMessage
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit
+from PyQt5.QtCore import Qt
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 class MyPillow(QWidget):
     def __init__(self):
         super().__init__()
+        self.flag = False
         self.initUI()
 
     def initUI(self):
@@ -14,7 +20,7 @@ class MyPillow(QWidget):
         self.setWindowTitle('Рисунок')
         self.btn = QPushButton('Показать', self)
         self.btn.move(10, 10)
-        self.btn.clicked.connect(self.f)
+        self.btn.clicked.connect(self.draw)
         self.lbl1 = QLabel('side', self)
         self.lbl1.move(120, 10)
         self.side = QLineEdit(self)
@@ -32,28 +38,42 @@ class MyPillow(QWidget):
         self.weight = 0
 
     def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        self.draw_rect(qp)
-        qp.end()
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_rect(qp)
+            qp.end()
 
     def draw_rect(self, qp):
-        x, y = 240 - (self.weight // 2), 350 - (self.weight // 2)
-        qp.setBrush(QColor(255, 0, 0))
-        for _ in range(self.n):
-            qp.drawRect(x, y, self.weight, self.weight)
-            self.weight = round(self.k * self.weight)
-            x = 240 - (self.weight // 2)
-            y = x + 110
+        try:
+            self.n = int(self.kolvo.text())
+            self.k = float(self.coeff.text())
+            self.weight = int(self.side.text())
+            if self.n <= 0:
+                raise ValueError
+            side = self.weight
+            if side <= 0:
+                raise ValueError
+            c = self.k
+            if not 0 <= c <= 1:
+                raise ValueError
+            x, y = self.width() // 2,  self.height() // 2
+            qp.setPen(QColor('red'))
+            qp.setBrush(Qt.NoBrush)
+            for _ in range(self.n):
+                qp.drawRect(x - side // 2, y - side // 2, side, side)
+                side = int(side * c)
+        except ValueError:
+            pass
 
-    def f(self):
-        self.n = int(self.kolvo.text())
-        self.k = float(self.coeff.text())
-        self.weight = int(self.side.text())
+    def draw(self):
+        self.flag = True
+        self.repaint()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyPillow()
     ex.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec())
